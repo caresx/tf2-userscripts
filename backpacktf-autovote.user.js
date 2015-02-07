a7bdd3035b56b95784b611415008e49e860fc551
caresx <tanxc.cares@gmail.com>
2015-02-07 18:00:38 +0100

    Create backpacktf-autovote.user.js

---
+++ backpacktf-autovote.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         backpack.tf autovote
+// @namespace    http://steamcommunity.com/id/caresx/
+// @version      0.1
+// @description  Automatically vote on backpack.tf upon loading a voting page
+// @author       cares
+// @match        http://backpack.tf/vote*
+// @grant        none
+// ==/UserScript==
+
+$(function () {
+  $('.label-belt').not('.label-white-text').closest('.vote').find('.vote-agree').not('.active').click();
+  $('.label-belt .label-white-text').closest('.vote').find('.vote-disagree').not('.active').click();
+});
